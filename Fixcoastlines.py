02bafbea41983c48ba03a76f430449bca773019f
Chris Rowan <allochthonous@users.noreply.github.com>
2024-02-05 15:33:18 -0500

    All coast segments and mirrors plotting correctly

--- Fixcoastlines.py
+++ Fixcoastlines.py
@@ -84,59 +84,95 @@ for coastline in coast_latlons:
 fig, ax = plt.subplots(1, 1, figsize=(16,16), dpi=300)
 ax.plot(polydata['x'], polydata['y'], color='grey')
 
+for thing in result:
+    for item in thing[0:-1]: #last sections don't neccessarily end at barrier!
+        ax.plot(item[:,0], item[:,1], color='green')
+        rotated_x=item[-1][1]
+        rotated_y=item[-1][0]
 
-# not working for part of second item - question is, which part? 
-# Looks like problem might be that one of the segments only has one point?
-# do we need to add a first point to stop gaps?
-for item in result:
-    xmaxes, xmins = [], []
-    ymaxes, ymins = [], []
-    alt1=item[0]
-    first_flipped=np.flip(item[0],0)
-    dx=first_flipped[:,0]-item[1][0][1]
-    dy=first_flipped[:,1]-item[1][0][0]
-    if np.max(alt1[:,0])> limit: # N side - E side rotation
-        to_add=np.column_stack((item[1][0][0]+dy,item[1][0][1]-dx))
-    elif np.max(alt1[:,1])> limit: # E side - N side rotation
-        to_add=np.column_stack((item[1][0][0]-dy,item[1][0][1]+dx))
-    elif np.min(alt1[:,0]) < - limit: # S side - W side rotation
-        to_add=np.column_stack((item[1][0][0]+dy,item[1][0][1]-dx))
-    elif np.min(alt1[:,1]) < - limit: # W side - S side rotation
-        to_add=np.column_stack((item[1][0][0]-dy,item[1][0][1]+dx))   
-    alt2=np.flip(to_add,0) # need to back calculate the first segment!
-    lastpoint=item[0][-1]
-    flag=1
-    for thing in item[1:]:
-        dx=thing[:,0]-lastpoint[1]
-        dy=thing[:,1]-lastpoint[0]
-        
-        # note transformation is with respect to previous segment
-        if np.max(thing[:,0])> limit: # N side - E side rotation
-            to_add=np.column_stack((lastpoint[0]+dy,lastpoint[1]-dx))
-        elif np.max(thing[:,1])> limit: # E side - N side rotation
-            to_add=np.column_stack((lastpoint[0]-dy,lastpoint[1]+dx))
-        elif np.min(thing[:,0]) < - limit: # S side - W side rotation
-            to_add=np.column_stack((lastpoint[0]+dy,lastpoint[1]-dx))
-        elif np.min(thing[:,1]) < - limit: # W side - S side rotation
-            to_add=np.column_stack((lastpoint[0]-dy,lastpoint[1]+dx))      
-        lastpoint=thing[-1]
-        if flag==1: #concatenate rotated with alt1 and non-rotated with alt2
-            alt1=np.concatenate((alt1,to_add))
-            alt2=np.concatenate((alt2,thing))
-            flag=-1
-        else: #concatenate non-rotated with alt1 and rotated with alt2
-            alt1=np.concatenate((alt1,thing))
-            alt2=np.concatenate((alt2,to_add))
-            flag=1
-    ax.plot(alt1[:,0], alt1[:,1], color='green')
-    ax.plot(alt2[:,0], alt2[:,1], color='orange')
+        dx=np.flip(item[:,0]-item[-1][0],0)
+        dy=np.flip(item[:,1]-item[-1][1],0)
+        # checks last 
+        if item[-1][0]> limit: # E side - N side rotation
+            rotseg=np.column_stack((rotated_x+dy,rotated_y-dx))
+        elif item[-1][1]> limit: # N side - E side rotation
+            rotseg=np.column_stack((rotated_x-dy,rotated_y+dx))
+        elif item[-1][0] < - limit: # W side - S side rotation
+            rotseg=np.column_stack((rotated_x+dy,rotated_y-dx))
+        elif item[-1][1] < - limit: # S side - W side rotation
+            rotseg=np.column_stack((rotated_x-dy,rotated_y+dx)) 
+        else: # not quite sure why there are these ones..
+            if item[0][0]> limit: # E side - N side rotation
+                rotseg=np.column_stack((rotated_x+dy,rotated_y-dx))
+            elif item[0][1]> limit: # N side - E side rotation
+                rotseg=np.column_stack((rotated_x-dy,rotated_y+dx))
+            elif item[0][0] < - limit: # W side - S side rotation
+                rotseg=np.column_stack((rotated_x+dy,rotated_y-dx))
+            elif item[0][1] < - limit: # S side - W side rotation
+                rotseg=np.column_stack((rotated_x-dy,rotated_y+dx))            
+        ax.plot(rotseg[:,0], rotseg[:,1], color='orange')
+    ax.plot(thing[-1][:,0], thing[-1][:,1], color='blue') # Don't care about flipping for final segment
+    
+  
 
 
 
+
+polydata=pd.read_csv('prettypolygon2.txt', sep='\t')        
+prettypoly=Polygon([(x,y) for x,y in zip(polydata['x'], polydata['y'])])
 
 fig, ax = plt.subplots(1, 1, figsize=(16,16), dpi=300)
 ax.plot(polydata['x'], polydata['y'], color='grey')
-for item in result:
-    for thing in item:
-        ax.plot(thing[:,0], thing[:,1], color='blue')
+
+result=[]
+for coastline in coast_latlons:
+    lon, lat=np.array(coastline[0].tolist()),np.array(coastline[1].tolist())
+    spil_x, spil_y=from_lonlat_to_spilhaus_xy(lon,lat)
+    # calculates change in xy values for adjacent points in linestring
+    diff=pd.DataFrame(np.column_stack(([a-b for a,b in zip(spil_x[1:],spil_x[:-1])], 
+                                  [a-b for a,b in zip(spil_y[1:],spil_y[:-1])])),
+                  columns=['dx','dy'])
+    # finds spillover points where xy changes a lot between adjacent points 
+    breaks=diff.index[((diff['dx']>threshold) | (diff['dx']<-threshold)) | ((diff['dy']>threshold) | (diff['dy']<-threshold))]
+    # if breaks exist split linestring data at breakpoints and plot individually
+    if len(breaks)>0:
+        broken_segments=[]
+        p=0
+        for b in breaks:
+            broken_segments.append(np.column_stack(([x for x in spil_x[p:b+1]],[y for y in spil_y[p:b+1]])))
+            p=b+1
+        broken_segments.append(np.column_stack(([x for x in spil_x[p:]],[y for y in spil_y[p:]]))) #remember the last segment!
+        result.append(broken_segments)
+        for item in broken_segments[:-1]: #last sections don't neccessarily end at barrier!
+            ax.plot(item[:,0], item[:,1], color='green', lw=0.2)
+            rotated_x=item[-1][1]
+            rotated_y=item[-1][0]
         
+            dx=np.flip(item[:,0]-item[-1][0],0)
+            dy=np.flip(item[:,1]-item[-1][1],0)
+            # checks last 
+            if item[-1][0]> limit: # E side - N side rotation
+                rotseg=np.column_stack((rotated_x+dy,rotated_y-dx))
+            elif item[-1][1]> limit: # N side - E side rotation
+                rotseg=np.column_stack((rotated_x-dy,rotated_y+dx))
+            elif item[-1][0] < - limit: # W side - S side rotation
+                rotseg=np.column_stack((rotated_x+dy,rotated_y-dx))
+            elif item[-1][1] < - limit: # S side - W side rotation
+                rotseg=np.column_stack((rotated_x-dy,rotated_y+dx)) 
+            else: # not quite sure why there are these ones..
+                if item[0][0]> limit: # E side - N side rotation
+                    rotseg=np.column_stack((rotated_x+dy,rotated_y-dx))
+                elif item[0][1]> limit: # N side - E side rotation
+                    rotseg=np.column_stack((rotated_x-dy,rotated_y+dx))
+                elif item[0][0] < - limit: # W side - S side rotation
+                    rotseg=np.column_stack((rotated_x+dy,rotated_y-dx))
+                elif item[0][1] < - limit: # S side - W side rotation
+                    rotseg=np.column_stack((rotated_x-dy,rotated_y+dx))            
+            ax.plot(rotseg[:,0], rotseg[:,1], color='orange', lw=0.2)
+        ax.plot(broken_segments[-1][:,0], broken_segments[-1][:,1], color='blue', lw=0.2)
+    else: # if no breaks, just use whole linestring
+       ax.plot(spil_x,spil_y, color='red', lw=0.2)  
+
+
+
+
